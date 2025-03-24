2f84ed3a0e74698cc3e97a15aa95467b8ee5e680
JoaoMarion <joaormarion@gmail.com>
2025-03-24 11:17:21 -0300

    feat: muita coisa

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Main.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas.Ninjas.Controller;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesRank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
